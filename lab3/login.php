ed4656f68c7cd69d0ff5b352f906d09ed12f3e6f
ikaazhar <ikasyafinyazhar@gmail.com>
2024-11-09 22:41:15 +0800

    Update login.php

--- lab3/login.php
+++ lab3/login.php
@@ -1,20 +1,40 @@
 <html>
     <head>
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <title>Login</title>
     </head>
     <body>
-        <h2>Login</h2>
-        <form action="login.php" method="POST">
-            <label for="name">Username:</label><br>
-            <input type="text" id="username" name="username"><br>
-            <label for="password">Password:</label><br>
-            <input type="password" id="password" name="password"><br>
-            <input type="submit" value="Register">
-        </form>
+        <div class="bg-image" style="background-image: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('utm_bg.jpg'); height: 100vh; background-size: cover; background-position: center;">
+            <div class="d-flex flex-column align-items-center justify-content-center" style="height: 100%;">
+                <img src="UTM-LOGO-FULL.png" alt="Logo" class="mb-3" style="width: 250px; height: auto;">
+                <!-- White box container -->
+                <div class="bg-white p-4 rounded shadow" style="width: 320px;">
+                    <h2 class="text-center text-dark mb-4">Login</h2>
+                    <form action="login.php" method="POST">
+                        <div class="form-group">
+                            <label for="username" class="text-dark">Username:</label>
+                            <input type="text" id="username" name="username" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="text-dark">Password:</label>
+                            <input type="password" id="password" name="password" class="form-control">
+                        </div>
+                        <div class="text-center">
+                            <input type="submit" value="Login" class="btn btn-primary mt-3">
+                        </div>
+                        <!-- Register link below the submit button -->
+                        <a href="register.php" class="d-block text-center mt-3 text-primary">Don't have an account? Register here</a>
+                    </form>
+                </div>
+            </div>
+        </div>
 
-        <a href="register.php">Don't have an account? Register here</a>
+        <!-- Bootstrap JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
+
 <?php
 session_start();
 include "db_conn.php";
